e41e07f52a58ccf553bce78a91d6d1803056c324
zaidandzakyi <zaidan.dzaky.i@gmail.com>
2021-10-23 12:50:24 +0700

    add calculator.py

---
+++ calculator.py
@@ -0,0 +1,54 @@
+#CALCULATOR
+
+print("\n===== CALCULATOR =====\n")
+
+options = ['+','-','*','/']
+loop = True
+while loop:
+    try :
+        data = float(input("Input First Number : "))
+    except ValueError:
+        print("Data Invalid...")
+        break
+    data2 = input("(+ | - | * | / ) : ")
+    if data2 not in options :
+        print("Invalid syntax...")
+        break
+    try:
+        data3 = float(input("Input second Number : "))
+    except ValueError:
+        print("Data Invalid...")
+        break
+    if data2 == "+":
+        value = data + data3
+        print("Value : ",value)
+        user_input = input("\nwanna try again ? yes/no : ")
+        if user_input == "yes":
+            continue
+        else :
+            break
+    elif data2 == "-":
+        value = data - data3
+        print("Value : ",value)
+        user_input = input("\nwanna try again ? yes/no : ")
+        if user_input == "yes":
+            continue
+        else :
+            break
+    elif data2 == "*":
+        value = data * data3
+        print("Value : ",value)
+        user_input = input("\nwanna try again ? yes/no : ")
+        if user_input == "yes":
+            continue
+        else :
+            break
+    elif data2 == "/":
+        value = data / data3
+        print("Value : ",value)
+        user_input = input("\nwanna try again ? yes/no : ")
+        if user_input == "yes":
+            continue
+        else :
+            break
+print("\nCalculator End...")
